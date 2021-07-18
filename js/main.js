4cdc5513145301b21dc89238a603bf12002daabe
yudong0114 <yukisaito114@gmail.com>
2021-07-18 23:49:15 +0900

    [update]#5

--- js/main.js
+++ js/main.js
@@ -8,11 +8,18 @@
   let vm = new Vue({
     el: '#app',
     data: {
+      newItem: '',
       todos: [
         'task1',
         'task2',
         'task3'
       ]
+    },
+    methods: {
+      addItem: function() {
+        this.todos.push(this.newItem);
+        this.newItem = '';
+      }
     }
   })
 })();
