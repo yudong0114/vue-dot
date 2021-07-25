454bbf0447fb137207bcfde8b2c2c21f485e8753
yudong0114 <yukisaito114@gmail.com>
2021-07-25 23:40:19 +0900

    [update]#15

--- components/js/main.js
+++ components/js/main.js
@@ -4,13 +4,24 @@
 
   // 複数の要素を含める場合、親要素で囲う
   let likeComponent = Vue.extend({
-    template: '<button>Like</button>'
+    template: '<button @click="countUp">Like {{ count }}</button>',
+    // コンポーネントのdataは関数で返す必要がある
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      countUp: function() {
+        this.count++;
+      }
+    }
   })
 
   let vm = new Vue({
     el: '#app',
     components: {
       'like-component': likeComponent
-    }   
+    }
   })
 })();
